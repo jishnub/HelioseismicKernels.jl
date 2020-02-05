b19565295faa8ccfb3a1dab1548de42622a525f6
Jishnu Bhattacharya <jishnuonline@gmail.com>
2020-02-05 15:10:06 +0400

    Files generated by PkgTemplates

---
+++ src/HelioseismicTimeDistanceSensitivityKernels.jl
@@ -0,0 +1,5 @@
+module HelioseismicTimeDistanceSensitivityKernels
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HelioseismicTimeDistanceSensitivityKernels
+using Test
+
+@testset "HelioseismicTimeDistanceSensitivityKernels.jl" begin
+    # Write your own tests here.
+end
